0f860895b3f51cce55465f700c83cb74a91fbbb4
Steve R <sbrichards@mit.edu>
2014-01-26 17:06:53 -0800

    fuck it, going with a modal

--- templates/addfeed.php
+++ templates/addfeed.php
@@ -166,16 +166,29 @@
 		    </div>
 	    </nav>
 			<div class="main-content row">
-				
-				<div class="input-group">
-				  <span class="input-group-addon">Feed Name</span>
-				  <input type="text" class="form-control" placeholder="Ex: The Ubyssey" style="width:250px;">
-				</div>
-				<div class="input-group">
-				  <span class="input-group-addon">Feed Source</span>
-				  <input type="text" class="form-control" placeholder="Ex: http://ubyssey.ca/feed" style="width:250px;">
-				</div>
-				<button type="button" class="btn btn-group-lg navbar-btn expander">Add Feed</button>
+				<!-- Button trigger modal -->
+				<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+				  Launch demo modal
+				</button>
+
+				<!-- Modal -->
+				<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+				  <div class="modal-dialog">
+				    <div class="modal-content">
+				      <div class="modal-header">
+				        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+				      </div>
+				      <div class="modal-body">
+				        ...
+				      </div>
+				      <div class="modal-footer">
+				        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				        <button type="button" class="btn btn-primary">Save changes</button>
+				      </div>
+				    </div><!-- /.modal-content -->
+				  </div><!-- /.modal-dialog -->
+				</div><!-- /.modal -->
 		   </div>
 
 	<script>
