5d3bf28bd7d64d8bd2684515a4f969345b689a3c
Steve R <sbrichards@mit.edu>
2014-01-26 17:18:10 -0800

    blah 2

--- templates/addfeed.php
+++ templates/addfeed.php
@@ -166,6 +166,7 @@
 		    </div>
 		
 	    </nav>
+		<div class="main-content">
 		<!-- Button trigger modal -->
 		<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
 		  Launch demo modal
@@ -189,7 +190,7 @@
 		    </div><!-- /.modal-content -->
 		  </div><!-- /.modal-dialog -->
 		</div><!-- /.modal -->
-
+</div>
 	<script>
     <!-- Bootstrap core JavaScript -->
     <script src="../bs/js/bootstrap.min.js"></script>
